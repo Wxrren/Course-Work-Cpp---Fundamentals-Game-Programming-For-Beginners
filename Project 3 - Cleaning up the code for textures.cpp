f78ce6bb67c7af8e6dd95ab27704c34143575982
Wxrren <131816114+Wxrren@users.noreply.github.com>
2023-05-04 16:13:44 +0100

    Update Project 3 - Cleaning up the code for textures.cpp

--- Project 3 - Cleaning up the code for textures.cpp
+++ Project 3 - Cleaning up the code for textures.cpp
@@ -1,10 +1,17 @@
 //Nebula Variables.
 Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-Rectangle nebulaRec {0.0, 0.0, nebula.width/8, nebula.height/8};
-Vector2 nebulaPos {windowWidth, windowHeight - nebulaRec.height};
+Rectangle nebulaRec {
+    0.0, 0.0, nebula.width/8,
+    nebula.height/8};
+Vector2 nebulaPos {windowWidth, 
+        windowHeight - nebulaRec.height};
 
 
 // Scarfy Variables
 Texture2D Scarfy = LoadTexture("textures/scarfy.png");
-Rectangle scarfyRec {0.0, 0.0, Scarfy.width/6, Scarfy.height = Scarfy.height};
-Vector2 scarfyPos {scarfyPos.x = windowWidth/2 - scarfyRec.width/2, scarfyPos.y = windowHeight - scarfyRec.height};
+Rectangle scarfyRec {
+    0.0, 0.0, Scarfy.width/6, 
+    Scarfy.height = Scarfy.height};
+Vector2 scarfyPos {
+    scarfyPos.x = windowWidth/2- scarfyRec.width/2, 
+    scarfyPos.y = windowHeight - scarfyRec.height};
