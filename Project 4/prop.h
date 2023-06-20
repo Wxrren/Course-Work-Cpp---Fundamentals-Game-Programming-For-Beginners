81b82dde3a0aa5e57489997e48fda9cd15319dba
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-06-20 21:40:23 +0100

    Prop header file

--- Project 4/prop.h
+++ Project 4/prop.h
@@ -8,11 +8,11 @@ class Prop
 public:
     Prop(Vector2 pos, Texture2D tex);
     void Render(Vector2 KnightPos);
+    Rectangle GetCollisionRec(Vector2 KnightPos);
 
 private:
 
     Vector2 worldPos{};
     Texture2D texture{};
     float scale{4.f};
-
 };
