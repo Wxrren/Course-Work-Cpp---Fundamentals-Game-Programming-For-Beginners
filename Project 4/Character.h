0fb17592d03cf9a7321acd2aac6ad47d50291329
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-06-28 20:32:15 +0100

    Updated character class header

--- Project 4/Character.h
+++ Project 4/Character.h
@@ -1,30 +1,14 @@
 #include "raylib.h"
-class Character
+#include "BaseCharacter.h"
+
+class Character : public BaseCharacter
 {
 
 public:
     Character(int winWidth, int winHeight);
-    Vector2 getworldPos() { return worldPos; }
     void tick(float deltaTime);
-    void undoMovement();
-    Rectangle GetCollisionRec();
+   
 
 private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle   {LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D run    {LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{};
-    // 1 : facing right, -1 : facing left
-    float rightleft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    int maxFrames{6};
-    float updateTime{1.f / 12.f};
-    float speed{4.f};
-    float width {};
-    float height{};
-    float scale{4.0f};
+ 
 };
