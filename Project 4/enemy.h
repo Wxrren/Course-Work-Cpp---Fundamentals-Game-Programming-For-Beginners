00dd2e583c17a60f598dad231dcc2319d9a636cb
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-06-28 20:33:17 +0100

    Updated Enemy header class

--- Project 4/enemy.h
+++ Project 4/enemy.h
@@ -1,31 +1,14 @@
 #include "raylib.h"
+#include "BaseCharacter.h"
 
-class Enemy
+class Enemy : public BaseCharacter
 {
 public:
     Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture);
-    Vector2 getworldPos() { return worldPos; }
     void tick(float deltaTime);
-    void undoMovement();
-    Rectangle GetCollisionRec();
+  
 
 private:
-    Texture2D texture{LoadTexture("characters/goblin_idle_spritesheet.png")};
-    Texture2D idle   {LoadTexture("characters/goblin_idle_spritesheet.png")};
-    Texture2D run    {LoadTexture("characters/goblin_run_spritesheet.png")};
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{};
-    // 1 : facing right, -1 : facing left
-    float rightleft{1.f};
-    // animation variables
-    float runningTime{};
-    int frame{};
-    int maxFrames{6};
-    float updateTime{1.f / 12.f};
-    float speed{4.f};
-    float width {};
-    float height{};
-    float scale{4.0f};
+
 };
 
