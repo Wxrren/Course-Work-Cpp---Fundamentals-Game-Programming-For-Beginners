e6e0be2d3a2438e75806facc6bf94783bd7dccc5
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-06-29 22:29:06 +0100

    Updated base character functions

--- Project 4/BaseCharacter.cpp
+++ Project 4/BaseCharacter.cpp
@@ -22,3 +22,25 @@ void BaseCharacter::undoMovement()
         height * scale
     };
      }
+
+    
+void BaseCharacter::tick(float deltaTime)
+{
+     worldPosLastFrame = worldPos;
+
+     // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.f;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw character
+    Rectangle source{frame * width, 0.f, rightleft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    Vector2 origin{};
+    DrawTexturePro(texture, source, dest, origin, 0.f, WHITE);
+}
