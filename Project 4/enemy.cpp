4068b007c94c0c02bfcdbfc0f4f353bfc49bfb22
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-06-29 22:32:17 +0100

    Updated enemy functions

--- Project 4/enemy.cpp
+++ Project 4/enemy.cpp
@@ -14,22 +14,6 @@ Enemy::Enemy(Vector2 pos, Texture2D idle_texture, Texture2D run_texture)
 
 void Enemy::tick(float deltaTime)
 {
-     worldPosLastFrame = worldPos;
-
-        // update animation frame
-    runningTime += deltaTime;
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.f;
-        if (frame > maxFrames)
-            frame = 0;
-    }
-
-    // draw character
-    Rectangle source{frame * width, 0.f, rightleft * width, height};
-    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
-    Vector2 origin{};
-    DrawTexturePro(texture, source, dest, origin, 0.f, WHITE);
+    BaseCharacter::tick(deltaTime);
 }
 
