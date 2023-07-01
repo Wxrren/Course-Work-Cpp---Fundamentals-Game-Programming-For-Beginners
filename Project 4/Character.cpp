60e1466552422d37e1a4db52eedb156af51d3e4a
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-07-01 17:42:19 +0100

    Updated Character cpp file

--- Project 4/Character.cpp
+++ Project 4/Character.cpp
@@ -4,42 +4,35 @@
 #include "BaseCharacter.h"
 
 
-Character::Character(int winWidth, int winHeight)
+Character::Character(int winWidth, int winHeight) :
+    windowWidth(winWidth),
+    windowHeight(winHeight)
 {
     width = texture.width / maxFrames;
     height = texture.height;
+}
 
-    //Set Screen Pos
-     screenPos = {
-        static_cast<float>(winWidth) / 2 - scale * (0.5f * width),
-        static_cast<float>(winHeight) / 2 - scale * (0.5f * height)};
+// ScreenPos function
+Vector2 Character::getScreenPos()
+{
+    return Vector2{
+        static_cast<float>(windowWidth) / 2 - scale * (0.5f * width),
+        static_cast<float>(windowHeight) / 2 - scale * (0.5f * height)
+    };
 }
 
 void Character::tick(float deltaTime)
 {
-    BaseCharacter::tick(deltaTime);
 
-    Vector2 direction{};
+
     if (IsKeyDown(KEY_A))
-        direction.x -= 1.0;
+        velocity.x -= 1.0;
     if (IsKeyDown(KEY_D))
-        direction.x += 1.0;
+        velocity.x += 1.0;
     if (IsKeyDown(KEY_W))
-        direction.y -= 1.0;
+        velocity.y -= 1.0;
     if (IsKeyDown(KEY_S))
-        direction.y += 1.0;
-    if (Vector2Length(direction) != 0.0)
-    {
-        // Set worldPos = worldPos + direction
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        // changes sprite direction depending on if left or right
-        direction.x < 0.f ? rightleft = -1.f : rightleft = 1.f;
-        texture = run;
-    }
-    else
-    {
-        texture = idle;
-    }
-
+        velocity.y += 1.0;
+BaseCharacter::tick(deltaTime);
 }
 
